9c0d33867f0a56eb5ce6c0189ebd14a3c089ae9f
jose <dev.pradma@outlook.com>
2021-04-03 10:11:28 -0500

    Punto inicial

---
+++ admin/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+define('RUTA', 'http://localhost/deeptech/sistema_facturacion/');
+
+//NOMBRE DE BASE DE DATOS
+$bd_config = [
+	'db_name' => 'facturacion',
+	'user' => 'root',
+	'pass' => ''
+];
+
+?>
